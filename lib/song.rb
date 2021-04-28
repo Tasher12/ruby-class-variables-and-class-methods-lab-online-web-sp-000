73bdeb1b562e19e786ad8d205f15a68e19e121fb
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-04-28 19:21:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 class Song
 
   @@count = 0 
+<<<<<<< HEAD
   @@genres = []
   @@artists = []
   
@@ -39,4 +40,25 @@ end
     @@artists.each_with_object(Hash.new(0)){|value, key| key[value] += 1}
   end 
   
+=======
+  @@genre = []
+  @@artist = []
+  
+  attr_accessor :name, :artist, :genres 
+  
+  def initialize(genre, artist)
+    @@count += 1 
+    @@genre = genre
+    @@artist = artist 
+  end 
+  
+  def self.count
+    @@count
+  end 
+  
+  def self.genres
+    @@genre.push(genre) unless @@genre.includes(genre)
+  end 
+    
+>>>>>>> cd4275a9c0f1c57b28d3d6315cf827b9e7030e3a
 end 
